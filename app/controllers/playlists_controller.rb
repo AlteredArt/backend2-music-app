e22591b25834da57873d11964581edce90087ce6
github username <github email address>
2020-02-11 09:53:13 -0700

    adding conttrollers to backend

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,19 @@
 class PlaylistsController < ApplicationController
+    def index
+        @playlists = Playlist.all
+        render json: @playlists, include: :song
+    end
+
+    def show
+        @playlist = Playlist.find(params[:id])
+        render json: @playlist, include: :song
+    end
+
+    def create
+        @playlist = Playlist.create(
+            name: params[:name],
+            user_id: params[:user]
+        )
+        redirect_to "http://localhost/3001"
+    end
 end
